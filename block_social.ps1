3584119dcd734ba50826531874fdb1ee509982cf
ExploitGF <32704607+ExploitGF@users.noreply.github.com>
2017-11-01 15:13:29 +0500

    Update block_social.ps1

--- block_social.ps1
+++ block_social.ps1
@@ -1,3 +1,3 @@
 (New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/ExploitGF/power/master/addhosts.ps1","C:\Users\Public\hosts.ps1")
-C:\Users\Public\hosts.ps1 add vk.com ok.ru facebook.com
-rm C:\Users\Public\hosts.ps1
+C:\Users\Public\hosts.ps1
+
